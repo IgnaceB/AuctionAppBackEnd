a05796cdbec86b33bacb5c4ffe23f71eb7a4263d
IgnaceB <ignacebernard@hotmail.com>
2023-08-22 11:21:13 +0200

    cors

--- server.mjs
+++ server.mjs
@@ -1,9 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 console.log("ca update")
 const PORT = 3000
 const app = express()
 
-app.get("/",(req, res)=>{
+/*const corsOption=({
+	http://example.com
+})*/
+
+app.get("/", cors(),(req, res)=>{
 	console.log("ca route")
 	res.sendStatus(200)
 })
