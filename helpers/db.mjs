3f66a3b16d38ffbb8f35e66dcf0487f07c07ab19
IgnaceB <ignacebernard@hotmail.com>
2023-08-22 13:30:58 +0200

    deploy test

--- helpers/db.mjs
+++ helpers/db.mjs
@@ -6,18 +6,21 @@ dotenv.config()
 
 
 const connection = async (myQuery)=>{
-	const client = new Client({
+	try {const client = new Client({
 	user: process.env.user,
 	host: process.env.host,
 	database: process.env.database,
 	password: process.env.password,
 	port: process.env.port,
-})
+}
 	const connect = await client.connect()
 	const response = await client.query(myQuery)
 	console.log(response.rows[0])
 	client.end()
-	return response
+	return response}
+	catch (err){
+		return err
+	}
 
 }
 
