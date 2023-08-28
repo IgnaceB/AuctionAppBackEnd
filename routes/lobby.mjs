cc98a70d902e8a921e78aecb2f883060c63df442
IgnaceB <ignacebernard@hotmail.com>
2023-08-28 14:47:06 +0200

    init lobby

--- routes/lobby.mjs
+++ routes/lobby.mjs
@@ -4,6 +4,51 @@ import {DateTime} from 'luxon'
 
 const router=express.Router()
 
+router.get('/init/:lobby_id',async (req,res)=>{
+	//retrieve the id of the lobby
+	try{
+		const currentLobby=req.params.lobby_id
+
+	//retrieve data from the lobby_id
+		const lobbyQuery = `select *from lobby where id='${currentLobby}'`
+		const lobby =await connect(lobbyQuery)
+
+			//retrieve data from items
+		const itemQuery=`select *from items where id=${lobby.rows[0]["id_item"]}`
+		const item = await connect(itemQuery)
+		
+		//retrieve data from pictures
+		const picturesQuery=`select *from items_pictures where id_item=${item.rows[0]["id"]}`
+		const pictures =await connect(picturesQuery)
+
+		//retrieve data from users
+		const sellerQuery=`select *from users where id=${item.rows[0]["id_seller"]}`
+		const seller =await connect(sellerQuery)
+
+		//retrieve data from chat 
+		const chatQuery=`select *from chat where id_lobby=${currentLobby}`
+		const chat =await connect(chatQuery)
+
+		//retrieve data from tags
+		const tagsQuery=`select *from items_tags where id_item=${item.rows[0]["id"]}`
+		const tags = await connect(tagsQuery)
+
+		res.status(200).json({
+			lobby : lobby.rows,
+			item : item.rows,
+			pictures : pictures.rows,
+			seller : seller.rows,
+			chat : chat.rows,
+			tags : tags.rows
+		})
+
+	}
+	catch(err){
+		console.log(err)
+		res.status(404).json({message:'connection error, contact webmaster'})
+	}
+})
+
 router.get('/:lobby_id',async (req,res)=>{
 	//retrieve the id of the lobby
 	try{
