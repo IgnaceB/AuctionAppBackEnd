6cd1f160625291de4b3c0e35bbf4509ae1890f6c
IgnaceB <ignacebernard@hotmail.com>
2023-08-28 12:33:26 +0200

    tendance lobby

--- routes/allLobby.mjs
+++ routes/allLobby.mjs
@@ -44,4 +44,27 @@ router.get('/:page',async (req,res)=>{
 }
 )
 
+router.get('/tendance/:page',async (req,res)=>{
+	try{
+		
+	//retrieve data from db for all the lobby and create field mycount with likes + message + bid
+		const currentPage=req.params.page
+		let nrEntity = 20
+		let startSearch = nrEntity*currentPage
+
+		const countQuery = `select *,(lobby.likes+
+		(select count (bid.id) as countbid from bid where bid.id_item=lobby.id_item)
+		+ (select count (id) as countchat from chat where chat.id_lobby=lobby.id)) as mycount 
+		from lobby order by mycount desc offset ${startSearch} limit ${nrEntity}`
+
+		const countTendance=await connect(countQuery)
+		res.status(201).json(countTendance.rows)			
+}
+	
+	catch(err){
+		console.log(err)
+		res.status(404).json({message:'connection error, contact webmaster'})
+	}
+})
+
 export default router
